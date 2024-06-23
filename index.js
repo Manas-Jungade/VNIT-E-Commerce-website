345f4b1a7302a52abd9907015ce247e20f8b0c86
Manas <manasjungade7@gmail.com>
2024-06-23 22:37:53 +0530

    Cart made successfully

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import session from "express-session";
 import env from "dotenv";
 import multer from "multer";
 import { v2 as cloudinary } from 'cloudinary';
+
 const app = express();
 const port = 3000;
 const saltRounds=10;
@@ -52,22 +53,55 @@ cloudinary.config({
     api_secret: process.env.api_secret
 });
 var curr_user;
+var notification_item=[];
 
 app.get("/", (req, res) => {
     //Step 1 - Make the get route work and render the index.ejs file.
     res.render("login.ejs");
 });
 app.get("/sell",(req,res)=>{
-    res.render("sell.ejs");
+    res.render("sell.ejs",{
+        length:notification_item.length
+    });
 
 })
 
-app.get("/notification",(req,res)=>{
-    res.render("notification.ejs");
+app.get("/notification",async (req,res)=>{
+   
+    try {
+        const notify= await countnotification();
+
+
+        res.render("notification.ejs",{
+            length:notify.length,
+            notification:notify
+        });
+     
+    } catch (error) {
+        console.log(error);
+
+    }
+
 
 })
+async function countnotification () {
+    try {
+        const result=await db.query("SELECT * FROM notifications INNER JOIN products ON product_id=prod_id INNER JOIN students ON students.id=customer_id WHERE seller_id=$1 ",[curr_user]);
+        const notify=[];
+        result.rows.forEach(element => {
+            notify.push(element);
+        });
+        notification_item=notify;
+        return  notify;
+    } catch (error) {
+        console.log(error);
+    }
+   
+}
 app.get("/about",(req,res)=>{
-    res.render("about.ejs");
+    res.render("about.ejs",{
+        length:notification_item.length
+    });
     
 })
 app.get("/login", (req, res) => {
@@ -92,26 +126,27 @@ app.get("/logout",(req,res)=>{
    
 })
 app.get("/index", async(req,res)=>{
+   
     console.log(req.user);
-
-
     if(req.isAuthenticated()){
         const current_user_id=req.user.id;
         curr_user=req.user.id;
-        console.log(current_user_id);
+       
         try {
-        const result=await db.query("SELECT * FROM products");
+        const notify = await countnotification();
+        const result=await db.query("SELECT * FROM products INNER JOIN students ON student_id=students.id");
         let prod=[];
         result.rows.forEach((p)=>{
             if(p.student_id!=current_user_id){
                 prod.push(p);
             }
         })
-        const user_result=await db.query("SELECT * FROM students WHERE (id)=$1",[curr_user]);
-        const user=user_result.rows[0];
+     
+
         res.render("index.ejs",{
             products:prod,
-            user:user
+            length:notify.length
+          
 
         });
 
@@ -123,6 +158,26 @@ app.get("/index", async(req,res)=>{
     }
     else{
         res.redirect("/login");
+    }
+})
+
+app.get("/cart",async (req,res)=>{
+    const id=curr_user;
+    try {
+        const result=await db.query("SELECT * FROM carts INNER JOIN products ON cart_product_id=prod_id WHERE cart_student_id=$1",[id]);
+        const carts=[];
+        result.rows.forEach(element => {
+             carts.push(element);
+        });
+        res.render("cart.ejs",{
+            carts:carts,
+            length:notification_item.length,
+            cart_len:carts.length
+        })
+
+
+    } catch (error) {
+        console.log(error);
     }
 })
 app.get("/auth/google",passport.authenticate("google",{
@@ -257,7 +312,60 @@ app.post("/sell",upload.single("pic"),async(req,res)=>{
     
 
 })
-  
+app.post("/buy",async (req,res)=>{
+    const product_id=req.body["product_id"];
+    const customer_id=curr_user;
+    try {
+        const res=await db.query("SELECT * FROM products WHERE (prod_id)=$1",[product_id]);
+        const seller_id=res.rows[0].student_id;
+
+        await db.query("INSERT INTO notifications (customer_id,product_id,seller_id) VALUES ($1,$2,$3)",[customer_id,product_id,seller_id]);
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+    
+
+    res.redirect("/index");
+
+})
+app.post("/marked", async (req,res)=>{
+    const notify_id=req.body["notify_id"];
+    try {
+        const res=await db.query("DELETE FROM notifications WHERE notify_id=$1",[notify_id]);
+
+    } catch (error) {
+        console.log(error);
+    }
+    res.redirect("/notification");
+
+})
+app.post("/cartremove",async (req,res)=>{
+    const cart=req.body["cart_id"];
+    try {
+        const res=await db.query("DELETE FROM carts WHERE cart_id=$1",[cart]);
+
+    } catch (error) {
+        console.log(error);
+
+    }
+    res.redirect("/cart");
+    
+})
+app.post("/cartadd",async(req,res)=>{
+    try {
+        const cart_id=req.body["cart_id"];
+        console.log(cart_id);
+        await db.query("INSERT INTO carts (cart_student_id,cart_product_id) VALUES ($1,$2)",[curr_user,cart_id]);
+        res.redirect("/cart");
+
+
+    } catch (error) {
+        console.log(error);
+
+    }
+})
 
 
 app.listen(port, () => {
